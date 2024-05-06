a3635682d39960a91a632b4f42558e90504d97fe
Radmir Musabaev <musabaevradmir@gmail.com>
2024-05-07 02:47:17 +0400

    исправил баг с swagger, добавил аутентификацию в swagger

--- src/main/java/com/digitalspirit/project/ProjectApplication.java
+++ src/main/java/com/digitalspirit/project/ProjectApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -18,7 +23,11 @@ public class ProjectApplication {
 
 	@Bean
 	public Docket productApi() {
+		List<SecurityScheme> schemeList = new ArrayList<>();
+		schemeList.add(new BasicAuth("basicAuth"));
+
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.digitalspirit.project"))
+				.build().securitySchemes(schemeList);
 	}
 }
