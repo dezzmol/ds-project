27568843b56f14e278800adcaf78acd4988a7e17
Radmir Musabaev <musabaevradmir@gmail.com>
2024-05-10 22:27:12 +0400

    добавил доп функции в сервисе пользователей

--- src/main/java/com/digitalspirit/project/user/repository/UserRepository.java
+++ src/main/java/com/digitalspirit/project/user/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.digitalspirit.project.user.repository;
 
+import com.digitalspirit.project.user.Role;
 import com.digitalspirit.project.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     Optional<User> findByUsername(String username);
+    Optional<List<User>> findAllByRole(Role role);
 }
--- src/main/java/com/digitalspirit/project/user/service/UserService.java
+++ src/main/java/com/digitalspirit/project/user/service/UserService.java
@@ -1,10 +1,15 @@
 package com.digitalspirit.project.user.service;
 
+import com.digitalspirit.project.exceptions.BadRequestException;
+import com.digitalspirit.project.user.Role;
 import com.digitalspirit.project.user.User;
 import com.digitalspirit.project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +22,14 @@ public class UserService {
 
     public void save(User user) {
         userRepository.save(user);
+    }
+
+    public List<User> getAdmins() {
+        return userRepository.findAllByRole(Role.ROLE_ADMIN)
+                .orElseThrow(() -> new BadRequestException(HttpStatus.NOT_FOUND, "Admins not found"));
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new BadRequestException(HttpStatus.NOT_FOUND, "User not found"));
     }
 }
