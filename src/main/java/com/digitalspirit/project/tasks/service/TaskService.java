017648116cadb87007c80cc5fad211d1d362d3bc
Radmir <musabaevradmir@gmail.com>
2024-05-11 01:55:01 +0400

    создал функцию для форка проекта

--- src/main/java/com/digitalspirit/project/tasks/service/TaskService.java
+++ src/main/java/com/digitalspirit/project/tasks/service/TaskService.java
@@ -1,10 +1,12 @@
 package com.digitalspirit.project.tasks.service;
 
+import com.digitalspirit.project.config.GitlabApiConfig;
 import com.digitalspirit.project.exceptions.BadRequestException;
 import com.digitalspirit.project.intern.entity.Intern;
 import com.digitalspirit.project.internship.entity.Internship;
 import com.digitalspirit.project.lessons.entity.Lessons;
-import com.digitalspirit.project.lessons.service.LessonsService;
+import com.digitalspirit.project.lessons.repository.LessonsRepository;
+import com.digitalspirit.project.message.service.MessageService;
 import com.digitalspirit.project.tasks.dto.CreateTaskDTO;
 import com.digitalspirit.project.tasks.dto.TaskDTO;
 import com.digitalspirit.project.tasks.entity.InternshipMembersTasks;
@@ -14,8 +16,11 @@ import com.digitalspirit.project.tasks.mapper.TaskMapper;
 import com.digitalspirit.project.tasks.repository.InternshipMemberTasksRepository;
 import com.digitalspirit.project.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Project;
+import org.gitlab4j.api.models.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,12 +30,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
-    private final LessonsService lessonsService;
+    private final LessonsRepository lessonsRepository;
     private final TaskMapper taskMapper;
     private final InternshipMemberTasksRepository internshipMemberTasksRepository;
+    private final MessageService messageService;
 
     public TaskDTO create(CreateTaskDTO taskDTO) {
-        Lessons lessons = lessonsService.findLessonById(taskDTO.getLesson_id());
+        Lessons lessons = lessonsRepository.findById(taskDTO.getLesson_id())
+                .orElseThrow(() -> new BadRequestException(HttpStatus.NOT_FOUND, "Lesson not found"));
 
         Task task = Task.builder()
                 .name(taskDTO.getName())
@@ -47,7 +54,8 @@ public class TaskService {
     }
 
     public List<TaskDTO> getTasksDTO(Long lessonId) {
-        Lessons lessons = lessonsService.findLessonById(lessonId);
+        Lessons lessons = lessonsRepository.findById(lessonId)
+                .orElseThrow(() -> new BadRequestException(HttpStatus.NOT_FOUND, "Lesson not found"));
 
         List<Task> tasks = taskRepository.findByLessons(lessons)
                 .orElseThrow(() -> new BadRequestException(HttpStatus.NOT_FOUND, "Tasks not found"));
@@ -61,13 +69,13 @@ public class TaskService {
     }
 
     public List<Task> getTasks(Long lessonId) {
-        Lessons lessons = lessonsService.findLessonById(lessonId);
+        Lessons lessons = lessonsRepository.findById(lessonId)
+                .orElseThrow(() -> new BadRequestException(HttpStatus.NOT_FOUND, "Lesson not found"));
 
         return taskRepository.findByLessons(lessons)
                 .orElseThrow(() -> new BadRequestException(HttpStatus.NOT_FOUND, "Tasks not found"));
     }
 
-    @Async
     public void openTasks(Internship internship, Long lessonId, List<Intern> interns) {
         List<Task> tasks = getTasks(lessonId);
         //TODO: Доработать решение за время меньшее чем O(tasksCount * internsCount)
@@ -75,13 +83,40 @@ public class TaskService {
             for (Task task : tasks) {
                 InternshipMembersTasks internshipMembersTasks = InternshipMembersTasks.builder()
                         .internship(internship)
-                        .task(task)
                         .intern(intern)
+                        .task(task)
                         .status(InternshipMembersTasksStatus.NOT_RESOLVED.name())
                         .build();
 
+                forkProject(task, intern);
                 internshipMemberTasksRepository.save(internshipMembersTasks);
             }
+        }
+    }
+
+    public void forkProject(Task task, Intern intern) {
+        try {
+            GitLabApi gitLabApi = GitlabApiConfig.gitlabApi();
+            String projectUrl = task.getLinkToRepository();
+            String username = intern.getUser().getUsername();
+
+            User user = gitLabApi.getUserApi().getUser(username);
+            if (user == null) {
+                messageService.createMessageForAdmins("User not found");
+                throw new GitLabApiException("User not found");
+            }
+
+            Project project = gitLabApi.getProjectApi().getProject(projectUrl);
+            if (project == null) {
+                messageService.createMessageForAdmins("Project not found");
+                throw new GitLabApiException("Project not found");
+            }
+
+            Project forkedProject = gitLabApi.getProjectApi().forkProject((Object) project.getId(), (String) null);
+
+        } catch (GitLabApiException e) {
+            messageService.createMessageForAdmins("Error while forking: " + e.getMessage());
+            ;
         }
     }
 }
