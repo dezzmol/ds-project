f821f35902f47ac03971afd2c2a454421bac9e8a
Radmir <musabaevradmir@gmail.com>
2024-05-11 01:55:21 +0400

    вынес токен в переменные окружения

--- src/main/java/com/digitalspirit/project/config/GitlabApiConfig.java
+++ src/main/java/com/digitalspirit/project/config/GitlabApiConfig.java
@@ -2,11 +2,13 @@ package com.digitalspirit.project.config;
 
 import lombok.Getter;
 import org.gitlab4j.api.GitLabApi;
+import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 public class GitlabApiConfig {
     private static final String host = "http://gitlab:8088";
-    private static final String token = ""; //TODO: add gitlab token
+    @Value("${gitlab.token}")
+    private static String token; //TODO: add gitlab token
 
     public static GitLabApi gitlabApi() {
         return new GitLabApi(host, token);
